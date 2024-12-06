bb309789b57d9fd19625b2c551069f2f1f322a00
Ishan Pranav <ishan.pranav@outlook.com>
2024-12-06 01:38:15 -0500

    Add day 6a

--- src/day06a.c
+++ src/day06a.c
@@ -2,4 +2,93 @@
 // Copyright (c) 2024-2025 Ishan Pranav
 // Licensed under the MIT license.
 
-// Print Queue
+// Guard Gallivant
+
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#define MAX_M 256
+#define MAX_N 256
+
+int main()
+{
+    char a[MAX_M][MAX_N];
+    size_t m = 0;
+    size_t n = 0;
+    size_t i = 0;
+    size_t j = 0;
+
+    if (fgets(a[0], MAX_N, stdin))
+    {
+        n = strlen(a[0]);
+
+        while (n > 0 && isspace(a[0][n - 1]))
+        {
+            n--;
+        }
+
+        m++;
+
+        while (fgets(a[m], MAX_N, stdin))
+        {
+            char* p = strchr(a[m], '^');
+
+            if (p)
+            {
+                i = m;
+                j = p - a[m];
+            }
+
+            m++;
+        }
+    }
+
+    int di = -1;
+    int dj = 0;
+    size_t k = 0;
+    bool b[MAX_M][MAX_N] = { 0 };
+
+    for (;;)
+    {
+        if (!b[i][j])
+        {
+            k++;
+            b[i][j] = true;
+        }
+
+        if (i + di >= m || j + dj >= n)
+        {
+            break;
+        }
+
+        while (a[i + di][j + dj] == '#')
+        {
+            if (di == 1)
+            {
+                di = 0;
+                dj = -1;
+            }
+            else if (di == -1)
+            {
+                di = 0;
+                dj = 1;
+            }
+            else if (dj == 1)
+            {
+                di = 1;
+                dj = 0;
+            }
+            else if (dj == -1)
+            {
+                di = -1;
+                dj = 0;
+            }
+        }
+
+        i += di;
+        j += dj;
+    }
+
+    printf("%zu\n", k);
+}
